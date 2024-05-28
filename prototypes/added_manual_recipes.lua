fc528f39d3808fd7a7e1ac0d2b14e996187d4361
Michael Schwarz <michi.schwarz@gmail.com>
2024-05-28 10:27:26 +0200

    Version 8.1.21

--- prototypes/added_manual_recipes.lua
+++ prototypes/added_manual_recipes.lua
@@ -7,3 +7,19 @@ if mods["pycoalprocessing"] then
 		table.insert(rf.custom_recycle, {"item", "empty-fuel-canister", "empty-jerry-can"})
 	end
 end
+
+--Added gas canister, gas barrel, energy cell, and kerosine from King Jo's Fuels mod
+if mods["kj_fuel"] then
+	if data.raw.item["kj_gascan"] and data.raw.recipe["kj_gascan_fill"] then
+		table.insert(rf.custom_recycle, {"item", "kj_gascan", "kj_gascan_fill"})
+	end
+	if data.raw.item["kj_energy_cell"] and data.raw.recipe["kj_energy_cell_load"] then
+		table.insert(rf.custom_recycle, {"item", "kj_energy_cell", "kj_energy_cell_load"})
+	end
+	if data.raw.item["kj_kerosine"] and data.raw.recipe["kj_kerosine_fill"] then
+		table.insert(rf.custom_recycle, {"item", "kj_kerosine", "kj_kerosine_fill"})
+	end
+	if data.raw.item["kj_gasbarrel"] and data.raw.recipe["kj_gasbarrel_fill"] then
+		table.insert(rf.custom_recycle, {"item", "kj_gasbarrel", "kj_gasbarrel_fill"})
+	end
+end
