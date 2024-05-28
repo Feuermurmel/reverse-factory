edb842407eb526c1fd989878aa829c2254c433a4
Michael Schwarz <michi.schwarz@gmail.com>
2024-05-28 10:27:12 +0200

    Version 3.3.1

--- control.lua
+++ control.lua
@@ -1,23 +1,22 @@
 function checkinvs()
-	local surface = game.surfaces['nauvis']
-    local recyclers = {}
-	--Find all reverse-factories
-	recyclers = surface.find_entities_filtered{name= "reverse-factory"}
-	if next(recyclers) then
-		for _, rf in pairs(recyclers) do
-			--Check if not currently recycling
-			if not rf.is_crafting() then
-				--Check if any items in the input
-				if rf.get_inventory(defines.inventory.assembling_machine_input).get_item_count() > 0 then
-					--Grab input contents, stored as table (pairs)
-					local items = rf.get_inventory(defines.inventory.assembling_machine_input).get_contents()
-					for key, num in pairs(items) do
-						--Squirrely-do to make a proper item table with strings
-						item = {name=key, count=num}
-						--And finally take the items and push them from input to output
-						if rf.get_output_inventory().can_insert(item) then
-							rf.get_output_inventory().insert(item)
-							rf.get_inventory(defines.inventory.assembling_machine_input).clear()
+	if next(rf.recyclers) then
+		for _, rf in pairs(rf.recyclers) do
+			--If recycler has power
+			if rf.energy > 0 then
+				--Check if not currently recycling
+				if not rf.is_crafting() then
+					--Check if any items in the input
+					if rf.get_inventory(defines.inventory.assembling_machine_input).get_item_count() > 0 then
+						--Grab input contents, stored as table (pairs)
+						local items = rf.get_inventory(defines.inventory.assembling_machine_input).get_contents()
+						for key, num in pairs(items) do
+							--Squirrely-do to make a proper item table with strings
+							item = {name=key, count=num}
+							--And finally take the items and push them from input to output
+							if rf.get_output_inventory().can_insert(item) then
+								rf.get_output_inventory().insert(item)
+								rf.get_inventory(defines.inventory.assembling_machine_input).clear()
+							end
 						end
 					end
 				end
@@ -26,8 +25,64 @@ function checkinvs()
 	end
 end
 
+script.on_init( function()
+	rf = {}
+	rf.recyclers = {}
+end)
+
+script.on_configuration_changed( function()
+	rf = {}
+	rf.recyclers = {}
+	for _, surface in pairs(game.surfaces) do
+		local recyclers = surface.find_entities_filtered{name= "reverse-factory"}
+		rf.recyclers = recyclers
+	end
+end)
+
 script.on_event(defines.events.on_tick, function(event)
 	if event.tick % 240 == 0 then
-		checkinvs()
+		if rf then checkinvs() end
+	end
+end)
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+	if event.created_entity.name == "reverse-factory" then
+		table.insert(rf.recyclers, event.created_entity)
+	end
+end)
+
+script.on_event(defines.events.on_built_entity, function(event)
+	if event.created_entity.name == "reverse-factory" then
+		table.insert(rf.recyclers, event.created_entity)
+	end
+end)
+
+script.on_event(defines.events.on_entity_died, function(event)
+	if event.entity.name == "reverse-factory" then
+		for key, entity in pairs(rf.recyclers) do
+			if entity == event.entity then
+				table.remove(rf.recyclers, key)
+			end
+		end
+	end
+end)
+
+script.on_event(defines.events.on_robot_pre_mined, function(event)
+	if event.entity.name == "reverse-factory" then
+		for key, entity in pairs(rf.recyclers) do
+			if entity == event.entity then
+				table.remove(rf.recyclers, key)
+			end
+		end
+	end
+end)
+
+script.on_event(defines.events.on_preplayer_mined_item, function(event)
+	if event.entity.name == "reverse-factory" then
+		for key, entity in pairs(rf.recyclers) do
+			if entity == event.entity then
+				table.remove(rf.recyclers, key)
+			end
+		end
 	end
 end)
