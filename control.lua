7eee78736674af8032b6805a783a592ec938c66f
Michael Schwarz <michi.schwarz@gmail.com>
2024-05-28 10:27:27 +0200

    Version 8.2.5

--- control.lua
+++ control.lua
@@ -1,61 +1,68 @@
 if settings.global["rf-compati"].value then
 
 function checkinvs()
-	--If at least one recycler in the list..
-	if next(global.recyclers) then
-		--Check through the entire list..
-		for key, entity in pairs (global.recyclers) do
-			--Resets the timer if the recycler is not stopped by input
-			local noResetTimer = true
-			--Entity[1] is the actual recycler, Entity[2] is its timer
-			--Then if the recycler has power..
-			if entity[1].energy > 0 then
-				--Then if the recycler is not currently recycling..
-				if not entity[1].is_crafting() then
-					--Then if the output is currently empty..
-					if entity[1].get_output_inventory().is_empty() then
-						--Then if the input is currently not empty..
-						if entity[1].get_inventory(defines.inventory.assembling_machine_input).get_item_count() > 0 then
-							--Decrement the timer, because the recycler is stopped with input
-							entity[2] = countdown(entity[2])
-							--Then if the timer has passed 0..
-							if entity[2] < 0 then
-								--Auto ingredient push and reset the timer if true
-								pushIngredient(entity[1])
-							--Do not reset the timer if the recycler is stopped by input
-							else noResetTimer = false
+	--Run through every currently active surface
+	for _, surface in pairs(global.activeSurfaces) do
+		--If at least one recycler in the list..
+		if global[surface] then if next(global[surface]) then
+			--Check through the entire list..
+			for _, entity in pairs (global[surface]) do
+				--Resets the timer if the recycler is not stopped by input
+				local noResetTimer = true
+				--Entity[1] is the actual recycler, Entity[2] is its timer
+				--Then if the recycler has power..
+				if entity[1].energy > 0 then
+					--Then if the recycler is not currently recycling..
+					if not entity[1].is_crafting() then
+						--Then if the output is currently empty..
+						if entity[1].get_output_inventory().is_empty() then
+							--Then if the input is currently not empty..
+							if entity[1].get_inventory(defines.inventory.assembling_machine_input).get_item_count() > 0 then
+								--Decrement the timer, because the recycler is stopped with input
+								entity[2] = countdown(entity[2])
+								--Then if the timer has passed 0..
+								if entity[2] < 0 then
+									--Auto ingredient push and reset the timer if true
+									pushIngredient(entity[1])
+								--Do not reset the timer if the recycler is stopped by input
+								else noResetTimer = false
+								end
 							end
 						end
 					end
 				end
+				--Resets the timer if the recycler was not stopped,
+				--or if the recycler has already pushed to output
+				if noResetTimer then
+					entity[2]=global.timer
+				end
 			end
-			--Resets the timer if the recycler was not stopped,
-			--or if the recycler has already pushed to output
-			if noResetTimer then
-				entity[2]=global.timer
-			end
-		end
+		end end
 	end
 end
 
 --Function to check the world for recyclers and place them in a list
 function scanworld()
-	--It's easier to remake the list of recyclers, than to check for duplicates on load
+	--It's easier to remake the list of recyclers, than to check for duplicates, on map load
 	global = {}
+	global.activeSurfaces = {}
 	global.delay = settings.global["rf-delay"].value
 	global.timer = settings.global["rf-timer"].value
-	global.recyclers = {}
 	--Check every game surface in the world
 	for _, surface in pairs(game.surfaces) do
 		for n=1, 4 do
 			--Check the list of entities for any placed recyclers
 			local rfname = "reverse-factory-"..n
 			local list = surface.find_entities_filtered{name=rfname}
-			--And add them to the list
+			--And add them to the list for that specific surface
 			for _, entity in pairs(list) do
-				addRecycler(entity)
+				addRecycler(entity, surface.name)
 			end
 		end
+	end
+	--Check all players in the world to see what surfaces are activeSurfaces
+	for _, player in pairs (game.players) do
+		addActiveSurface(player.surface.name)
 	end
 end
 
@@ -70,56 +77,95 @@ end)
 
 --Every 15 ticks, do a thing
 script.on_event(defines.events.on_tick, function(event)
-	if event.tick % global.delay == 0 then
-		--game.players[1].print((#global.recyclers))
-		--game.players[1].print(serpent.block(global.recyclers))
-		checkinvs()
+	if global.delay then
+		if event.tick % global.delay == 0 then
+			--game.players[1].print((#global.recyclers))
+			--game.players[1].print(serpent.block(global.recyclers))
+			checkinvs()
+		end
+	--else game.players[1].print(("Global delay not yet set."))
 	end
+	--game.players[1].print((serpent.block(global)))
 end)
 
 --When a recycler is placed, add it to the list
 script.on_event(defines.events.on_robot_built_entity, function(event)
-	addRecycler(event.created_entity)
+	addRecycler(event.created_entity, event.created_entity.surface.name)
 end)
 
 script.on_event(defines.events.on_built_entity, function(event)
-	addRecycler(event.created_entity)
+	--game.players[1].print((serpent.block(event.created_entity.surface.name)))
+	addRecycler(event.created_entity, event.created_entity.surface.name)
 end)
 
 --When a recycler is removed or destroyed, remove it from the list
 script.on_event(defines.events.on_entity_died, function(event)
-	killRecycler(event.entity)
+	killRecycler(event.entity, event.entity.surface.name)
 end)
 
 script.on_event(defines.events.on_robot_pre_mined, function(event)
-	killRecycler(event.entity)
+	killRecycler(event.entity, event.entity.surface.name)
 end)
 
 script.on_event(defines.events.on_robot_mined_entity, function(event)
-	killRecycler(event.entity)
+	killRecycler(event.entity, event.entity.surface.name)
 end)
 
 script.on_event(defines.events.on_pre_player_mined_item, function(event)
-	killRecycler(event.entity)
+	killRecycler(event.entity, event.entity.surface.name)
+end)
+
+--When a player changes surfaces (Warptorio 2)
+script.on_event(defines.events.on_player_changed_surface, function(event)
+	removeActiveSurface(game.surfaces[event.surface_index].name)
+	addActiveSurface(game.players[event.player_index].surface.name)
 end)
 
 --Check if the entity was a recycler, and if so, add it to the list with its own timer
-function addRecycler(entity)
+function addRecycler(entity, surface)
 	if string.find(entity.name, "reverse") and string.find(entity.name, "factory") then
 		local timer = global.timer
 		local new_entity = {entity,timer}
-		table.insert(global.recyclers,new_entity)
+		--If the list does not yet exist for this surface, create the list first
+		if not global[surface] then
+			global[surface] = {}
+		end
+		table.insert(global[surface],new_entity)
 	end
 end
 
 --Check if the entity was a recycler, and if so, remove it from the list
-function killRecycler(entity)
+function killRecycler(entity, surface)
 	if string.find(entity.name, "reverse") and string.find(entity.name, "factory") then
-		for key, list_entity in pairs (global.recyclers) do
+		for key, list_entity in pairs (global[surface]) do
 			if list_entity[1] == entity then
-				table.remove(global.recyclers,key)
+				table.remove(global[surface],key)
 			end
 		end
+	end
+end
+
+function addActiveSurface(surface)
+	local addSurface = true
+	for _, oldSurface in pairs (global.activeSurfaces) do
+		if string.match(surface, oldSurface) then
+			addSurface = false
+		end
+	end
+	if addSurface then
+		table.insert(global.activeSurfaces, surface)
+	end
+end
+
+function removeActiveSurface(surface)
+	local removeSurface = false
+	for key, oldSurface in pairs (global.activeSurfaces) do
+		if string.match(surface, oldSurface) then
+			removeSurface = true
+		end
+	end
+	if removeSurface then
+		table.remove(global.activeSurfaces, key)
 	end
 end
 
